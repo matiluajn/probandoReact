214d3894b39292993e30784285d94464c9e162cb
Matias Lujan <emilianomatias87@gmail.com>
2022-01-28 12:46:29 -0300

    inicial commit global

---
+++ reactpfjs/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Primer proyecto levantando <strong>REACT!</strong>
+        </p>
+        <a
+          className="App-link"
+          href="https://matiluajn.github.io/Pollo-fried-by-nahue/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Presiona aqui para ver mi proyecto fonal de JS
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
