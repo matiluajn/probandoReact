c031c8156f059d41d561e74541cb9868dbe2fe36
Matias Lujan <emilianomatias87@gmail.com>
2022-02-06 13:30:35 -0300

    comit clase 4

--- reactpfjs/src/components/NavBar.js
+++ reactpfjs/src/components/NavBar.js
@@ -1,18 +1,37 @@
 import React from "react";
 import './NavBar.css';
+import logo from "./carrito-de-supermercado.png"
 
 function navBar(){
     return(
-        <header className="header">
-            <nav className="nav d-flex justify-content-center ">   
-                <nav class="nav   ">
-                    <a class="nav-link active text-danger" aria-current="page" href="#">Home</a>
-                    <a class="nav-link text-danger" href="#">Abcdbout us</a>
-                    <a class="nav-link text-danger" href="#">Products</a>
-                    <a class="nav-link text-danger ">Cart</a>
-                </nav>
-            </nav>
-
+        <header className="header ">
+         <nav class="navbar navbar-expand-lg  ">
+            <div class="container-fluid">
+                <a class="navbar-brand" target="_blank" href="https://www.linkedin.com/in/matt-lujan">Matt-lujan</a>
+                   
+                    
+                 <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ">
+                        <a class="nav-link " href="#">Features</a>
+                        </li>
+                        <li class="nav-item ">
+                        <a class="nav-link " href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item ">
+                        <a class="nav-link " href="#">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+                <a href="">
+                    <img className="cart-logo" src={logo}>
+                    </img>
+                </a>
+            </div>
+        </nav>
         </header>
     )
 }
